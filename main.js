24427ddc90a0b332d95286811edaadbae00e0c6f
Jaime Revello <srasrv@hotmail.com>
2021-06-15 16:04:19 +0200

    Skeleton

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
+const secrets = require("./conf/secrets.js");
 const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.once("ready", () => {
+  console.log("Botjima Online");
+});
+
+client.login(secrets.token);
